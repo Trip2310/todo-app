329362e91af2ee01bdf5f0855fb08ace0e1d3e33
Trip2310 <113469900+Trip2310@users.noreply.github.com>
2023-01-30 11:00:25 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,83 @@
+import React,{useState} from 'react'
+import './App.css';
+
+
+
+const App = () => {
+const[todo, setTodo]= useState("");             // for single todo
+const[todos,setTodos]=useState([]);   //// for all todos
+const[ editId,setEditId]= useState(0);            
+
+const handleSubmit=(e)=>{
+e.preventDefault();      
+
+if(editId){
+  const editTodo=todos.find((i)=>i.id===editId);
+  const updatedTodos=todos.map((t)=>t.id===editTodo.id
+  ?(t={id:t.id,todo})
+  :{id:t.id, todo:t.todo}
+);
+setTodos(updatedTodos);
+setEditId(0);
+setTodo("");
+return;
+}
+if(todo!==''){
+  setTodos([{id:`${todo}-${Date.now()}` ,todo},...todos]);  // meke uniqe id use template literal, date.now always give the time in millisecond, ...todos are spread oprator
+  setTodo("");        
+}
+};       
+
+const handleDelete =(id)=>{
+const delTodo = todos.filter((to)=>to.id !==id);  
+setTodos([...delTodo]);                 
+};
+
+const handleEdit=(id)=>{        
+const editTodo=todos.find((i)=>i.id===id);
+setTodo(editTodo.todo);
+setEditId(id);
+}
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>Todo List App</h1>
+
+        <form className='todoForm' onSubmit={handleSubmit}>   
+          <input type='text'value={todo} onChange ={(e)=> setTodo(e.target.value)}/> 
+          <button type="submit">{editId? "Edit" :"Add" }</button>
+        </form>
+
+        <ul className='allTodos'>
+          {
+            todos.map((t)=>(             
+          <li className='singleTodo'>
+            <span className='todoText'key={t.id}>{t.todo}</span>
+            <button onClick={()=>handleEdit(t.id)}>Edit</button>
+            <button onClick={()=>handleDelete(t.id)}>Delete</button>
+          </li>
+
+          ))}
+        </ul>
+
+
+
+
+
+
+
+
+      </div>
+        
+      
+
+
+
+      
+
+
+    </div>
+  )
+}
+
+export default App
